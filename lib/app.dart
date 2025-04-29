ab1bf8aade53de5c25568c60854788ed054f0237
Jobaieer <jobaieer.cse@proton.me>
2025-04-29 21:49:40 +0600

    feat: Initialize Home screen scaffold with AppBar

---
+++ lib/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Food Lagbe",
+      home: Home(),
+
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('Food Lagbe', style: TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),),
+        titleSpacing: 0,
+        centerTitle: true,
+        leading: Icon(Icons.menu, color:  Colors.white),
+        actions: [
+          IconButton(onPressed: () => {}, icon: Icon(Icons.search, color:  Colors.white))
+        ],
+      ),
+
+    );
+  }
+}
